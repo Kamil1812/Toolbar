254f6e0ed4de2ee8bffc5fe237ddb9393b04afcf
Filippo0420 <fpawelec0@gmail.com>
2023-04-04 08:07:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/toolbar/inf_uzytkownik.kt
@@ -0,0 +1,12 @@
+package com.example.toolbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class inf_uzytkownik : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inf_uzytkownik)
+    }
+}
---
+++ app/src/main/java/com/example/toolbar/logowanie.kt
@@ -0,0 +1,16 @@
+package com.example.toolbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_login).setOnClickListener {
+
+        }
+    }
+}
