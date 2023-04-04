7fa2f04574f230da0f32380693b5ec9066389398
Filippo0420 <fpawelec0@gmail.com>
2023-04-04 08:16:17 +0200

    Logowanie

--- app/src/main/java/com/example/toolbar/logowanie.kt
+++ app/src/main/java/com/example/toolbar/logowanie.kt
@@ -3,6 +3,8 @@ package com.example.toolbar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +12,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.btn_login).setOnClickListener {
+            val login = findViewById<EditText>(R.id.editText_login).text.toString()
+            val haslo = findViewById<EditText>(R.id.editText_haslo).text.toString()
 
+            if (!login.isEmpty() && !haslo.isEmpty())
+            {
+                findViewById<TextView>(R.id.textView_error).text = ""
+
+                if (login == "Kamil" && haslo == "Niewiadomski")
+                {
+
+                }
+            }
+
+            else
+            {
+                findViewById<TextView>(R.id.textView_error).text = "Podaj wszystkie dane!"
+            }
         }
     }
 }
